b030ac7219fdd1fa3277e0aa73fd072e47496365
lipan <670339228@qq.com>
2025-05-23 11:06:28 +0800

    修复代码

---
+++ src/settings.9ebc8.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/Loading.fire",orientation:"portrait",jsList:["assets/script/utils/jszip.min.39c63.js"],bundleVers:{internal:"468ae",resources:"d03b4",main:"e485b"}};
